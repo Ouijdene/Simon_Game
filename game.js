ff08612c8f4ff1c3febf887a793de9d643c1c137
Ouijdene <ouijdene.derouiche@gmail.com>
2022-02-23 20:21:10 +0100

    Complete part 1

---
+++ game.js
@@ -0,0 +1,45 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keydown(function () {
+  if (!started) {
+    $("#level-title").html("Level " + level);
+    nextSequence();
+    stared = true;
+  }
+});
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+  level++;
+  $("#level-title").html("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
