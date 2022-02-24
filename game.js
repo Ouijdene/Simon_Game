73f2665a81cfd635c20ec33b4bde030b704cb7ba
Ouijdene <ouijdene.derouiche@gmail.com>
2022-02-24 12:43:11 +0100

    complete Simon game

--- game.js
+++ game.js
@@ -6,9 +6,9 @@ var started = false;
 
 $(document).keydown(function () {
   if (!started) {
-    $("#level-title").html("Level " + level);
+    $("#level-title").text("Level " + level);
     nextSequence();
-    stared = true;
+    started = true;
   }
 });
 
@@ -17,9 +17,31 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      console.log("success");
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").html("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
@@ -37,9 +59,15 @@ function playSound(name) {
   audio.play();
 }
 
-function animatePress(currentColour) {
-  $("#" + currentColour).addClass("pressed");
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
-    $("#" + currentColour).removeClass("pressed");
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = 0;
 }
